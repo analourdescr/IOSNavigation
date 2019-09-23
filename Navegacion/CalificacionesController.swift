a788ab33282051489d8a98e1d0fc48c2bb13a4f2
Ana <ulsa@Jenny.local>
2019-09-23 14:50:00 -0700

    first commit

---
+++ Navegacion/CalificacionesController.swift
@@ -0,0 +1,34 @@
+//
+//  CalificacionesController.swift
+//  Navegacion
+//
+//  Created by Alumno on 9/23/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CalificacionesController : UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var materias : [Materia] = []
+    override func viewDidLoad() {
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return materias.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celdaMateria")
+        celda?.textLabel?.text = materias[indexPath.row].nombre
+        return celda!
+    }
+    
+    
+}
